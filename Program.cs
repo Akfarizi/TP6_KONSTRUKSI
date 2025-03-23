dc9660aaadcbbd3a6173a030905d6fa2489a8778
Akfarizi <muhammadalfarizi0204@gmail.com>
2025-03-24 01:44:52 +0700

    Menambahkan main program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using tpmodul6_103022300078;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – MUHAMMAD_AKBAR_ALFARIZI");
+
+        video.IncreasePlayCount(100);
+
+        video.PrintVideoDetails();
+    }
+}
